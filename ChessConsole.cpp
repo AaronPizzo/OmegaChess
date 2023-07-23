bc56ed50012f5c8e5a370a97cb891d20fb227871
AaronPizzo <Aaronpizzo@gmail.com>
2023-07-23 12:26:49 -0700

    C++ chess engine start

---
+++ ChessConsole.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+
+
+
+
+
+
+
+	const int Knight = 3;
+	cout << "a knight is " << Knight << " Points \n ";;
+
+	const int Bishop = 3;
+	cout << "a bishop is " << Bishop << " Points \n ";;
+
+
+	int Number;
+	cout << "What level of Omega would you like to play? ";
+	cin >> Number;
+	cout << "Starting level \n\n" << Number;
+
+
+
+	int x, y;
+	int sum;
+	cout << "\n Type a number: ";
+	cin >> x;
+	cout << "Type another number: ";
+	cin >> y;
+	sum = x + y;
+	cout << "Sum is: \n \n" << sum;
+
+
+
+	int main(); {
+
+		int MyName;
+		int name = 4;
+		cout << "What is your name?";
+		cin >> MyName;
+		if (MyName = name) {
+			cout << "Hello Aaron!";
+		}
+		else {
+			cout << "Sorry! But that is the wrong name";
+		}
+
+
+
+		return 0;
+	}
+
+
+}
+
+
+
+
+// Remember to end line to continue code;
